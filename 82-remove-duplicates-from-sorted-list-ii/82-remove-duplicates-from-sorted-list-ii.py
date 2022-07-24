22ae02c3867d89085438999ad97b6cfa15765636
Satendra Kumar <94552768+Chin2uuu@users.noreply.github.com>
2022-07-24 19:06:54 +0530

    Time: 110 ms (5.07%), Space: 13.9 MB (73.81%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -14,13 +14,14 @@ class Solution:
 
 
         while(r):
-            if l.val==r.val:
+            if l.val==r.val:# if l val and r val are equal move r to r.next
+                
                 r=r.next
-            elif l.next==r:
+            elif l.next==r: # checking distacne weather l.next if equal value of r then we conclude l to the n.next or ll and move the pointer l to r and continue the process
                 n.next=l
                 n=l
                 l=r
-            else:
+            else:  # if any of the condition would not matched then we will surly move l to r 
                 l=r
-        n.next=None if l.next else l
-        return head.next
+        n.next=None if l.next else l # after the while loop got executed then we have to check weather l.next exist or not if yes then n.next surly point to the null other wise conclued l itself in the returning head
+        return head.next# just to avoid the dummy node returning the head.next
