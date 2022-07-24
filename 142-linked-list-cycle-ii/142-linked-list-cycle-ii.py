666d21176c081639e5fb422afe1c5e74b5cc157f
Satendra Kumar <94552768+Chin2uuu@users.noreply.github.com>
2022-07-24 18:20:03 +0530

    Time: 46 ms (98.53%), Space: 17.3 MB (94.27%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -8,13 +8,13 @@ class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
        
         slow,fast=head,head
-        while(fast and fast.next):
+        while(fast and fast.next): # here we are finding weather the ll has cycle or not 
             slow=slow.next
             fast=fast.next.next
             if fast is slow:
                 fast=head
-                break
-        if fast and fast.next:
+                break #If cycle is found than defenitly we set fast pointer at head and traverse though out the poblem
+        if fast and fast.next: #if fast may be not exitst the program will return none but if the fast.next is exist the the program will check cycle for sure and return the postion were the slow and fast pointer are equal 
             
             while slow!=fast:
                 fast=fast.next
