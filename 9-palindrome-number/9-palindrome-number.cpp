310fe4aa3ca2a424d8887ca4ce5b8f62925c359f
Satendra Kumar <94552768+satendrakumar01@users.noreply.github.com>
2022-10-07 00:52:35 +0530

    Time: 8 ms (96.22%), Space: 6 MB (30.61%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -8,10 +8,10 @@ public:
             sum = sum*10+x%10;
             x = x/10;
         }
-        cout<<(x==sum)<<endl;
-        cout<<(x==sum/10)<<endl;
-        cout<<x<<endl;
-        cout<<sum<<endl;
+        // cout<<(x==sum)<<endl;
+        // cout<<(x==sum/10)<<endl;
+        // cout<<x<<endl;
+        // cout<<sum<<endl;
         return (x==sum)||(x==sum/10);
 //         here in case of pailndrome x==sum/10 is always equal;
     
